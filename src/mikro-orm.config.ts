55c599156bf5342d54de16072c62863cb22fa674
Damjan Piperski <damjanpiperski@gmail.com>
2021-12-16 13:45:09 +0100

    init create posts

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,13 +1,15 @@
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
   entities: [Post],
   dbName: 'fullstack',
   migrations: {
-    path: './migrations',
-    pattern: /^[\w-]+\d+\.ts$/,
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+    disableForeignKeys: false,
   },
   type: 'postgresql',
   user: 'postgres',
