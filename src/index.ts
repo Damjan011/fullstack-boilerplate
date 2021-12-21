045df8e8bb452c40ff93ef6bea8cda27f3043d4c
Damjan011 <damjanpiperski@gmail.com>
2021-12-21 15:58:29 +0100

    express types

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "graphql";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  // const apolloServer = new ApolloServer({
+  //   schema: await buildSchema({
+  //     resolvers: [HelloResolver, PostResolver]
+  //   })
+  // })
   app.get('/', (_, res) => {
     res.send("hello");
   });
