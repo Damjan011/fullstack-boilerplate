d7f7ababf114ba7d881014bb1f83d9233becbefa
Damjan011 <damjanpiperski@gmail.com>
2021-12-23 16:50:34 +0100

    update and delete

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -11,7 +11,7 @@ export class PostResolver {
 
   @Query(() => Post, { nullable: true })
   post(
-    @Arg("id", () => Int) id: number,
+    @Arg("id") id: number,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
@@ -25,5 +25,31 @@ export class PostResolver {
     const post = em.create(Post, { title });
     await em.persistAndFlush(post);
     return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
   }
 }
