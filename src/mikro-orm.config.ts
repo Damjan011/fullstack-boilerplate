7658f4201b626b59cae596dcfc0da9ebc2e30938
Damjan011 <damjanpiperski@gmail.com>
2021-12-27 16:46:33 +0100

    user resolver

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,9 +2,10 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'fullstack',
   migrations: {
     path: path.join(__dirname, "./migrations"),
