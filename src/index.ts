7d6239eb1756f68f7d9d01a131ca5667043675b7
Damjan011 <damjanpiperski@gmail.com>
2021-12-31 18:19:39 +0100

    typo

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const main = async () => {
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
-    console.log('started on localhost/:4000')
+    console.log('started on localhost:4000')
   });
 };
 
